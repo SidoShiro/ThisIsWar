96141056a8ea6c7cc683dfce85b9a0258f3b0de4
sidore_m <marc.sidorenko@epita.fr>
2018-03-07 14:05:09 +0100

    Cam Follow

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+  public GameObject to_follow;
+  public float yAx = 30;
+
+  private Vector3 to_move;
+
+  void Start()
+  {
+    to_move.y = yAx;
+  }
+
+	// Update is called once per frame
+	void Update ()
+  {
+    to_move.x = to_follow.transform.position.x;
+    to_move.z = to_follow.transform.position.z;
+    transform.position = to_move;
+	}
+}
